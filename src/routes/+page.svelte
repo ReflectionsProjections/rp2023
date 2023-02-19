34c89a4ffab5bcf3c6a272971770aaf133efa881
Soham Kulkarni <sohamkrox@gmail.com>
2023-02-19 17:37:10 -0600

    Edit landing page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="bg-slate-600 w-screen h-screen flex justify-center items-center flex-col gap-3">
-	<div class=" text-white text-3xl font-mono">Reflections | Projections</div>
-	<div class=" text-gray-200 text-3xl font-mono">2023</div>
+	<div class=" text-white text-3xl font-mono">Reflections | Projections 2023</div>
+	<div class=" text-gray-200 text-3xl font-mono">Coming Soon</div>
 </div>
