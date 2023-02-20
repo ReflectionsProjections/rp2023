9d0490ed8f17d82f9c8d2a342bb6da5449c1612e
Soham Kulkarni <sohamkrox@gmail.com>
2023-02-19 18:04:08 -0600

    Add fluid background gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<div class="bg-slate-600 w-screen h-screen flex justify-center items-center flex-col gap-3">
-	<div class=" text-white text-3xl font-mono">Reflections | Projections 2023</div>
+<div class="fluid-gradient w-screen h-screen flex justify-center items-center flex-col gap-3">
+	<div class=" text-white text-xl md:text-3xl font-mono text-center">
+		Reflections | Projections 2023
+	</div>
 	<div class=" text-gray-200 text-3xl font-mono">Coming Soon</div>
 </div>
