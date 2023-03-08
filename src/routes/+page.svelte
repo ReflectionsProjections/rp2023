44f9a5e86c6eb07b7599da7567c6c8a603fe90c3
Soham Kulkarni <sohamkrox@gmail.com>
2023-03-07 23:56:58 -0600

    Add link to 2022 site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
 <div class="fluid-gradient w-screen h-screen flex justify-center items-center flex-col gap-3">
-	<div class=" text-white text-xl md:text-3xl font-mono text-center">
-		Reflections | Projections 2023
-	</div>
+	<div class=" text-white text-3xl font-mono text-center">Reflections | Projections 2023</div>
 	<div class=" text-gray-200 text-3xl font-mono">Coming Soon</div>
+
+	<a
+		href="https://2022.reflectionsprojections.org/"
+		class="flex flex-row gap-2 items-center bg-white opacity-80 rounded-md p-3 px-4 my-5 hover:opacity-90 duration-200"
+	>
+		<div>Visit 2022 Site</div>
+		<Icon icon="mdi:arrow-top-right-thin" />
+	</a>
 </div>
