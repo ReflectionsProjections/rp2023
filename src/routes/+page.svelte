cf948423a00bbc50a3cd1b709fb025d3ddd1464f
Soham Kulkarni <sohamkrox@gmail.com>
2023-03-29 22:54:13 -0500

    Update temporary landing page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,25 @@
 	import Icon from '@iconify/svelte';
 </script>
 
-<div class="fluid-gradient w-screen h-screen flex justify-center items-center flex-col gap-3">
-	<div class=" text-white text-3xl font-mono text-center">Reflections | Projections 2023</div>
-	<div class=" text-gray-200 text-3xl font-mono">Coming Soon</div>
-
-	<a
-		href="https://2022.reflectionsprojections.org/"
-		class="flex flex-row gap-2 items-center bg-white opacity-80 rounded-md p-3 px-4 my-5 hover:opacity-90 duration-200"
-	>
-		<div>Visit 2022 Site</div>
-		<Icon icon="mdi:arrow-top-right-thin" />
-	</a>
+<div class="h-screen text-white flex items-center">
+	<span class="flex flex-col md:flex-row mx-auto">
+		<div class="flex flex-col text-3xl m-5 font-serif">
+			<div class="whitespace-nowrap">Reflections | Projections</div>
+			<div>2023</div>
+			<div class="text-xl my-3">Coming Soon</div>
+		</div>
+		<div class="flex flex-col m-5 w-96 font-sans">
+			<div class="text-lg">
+				The largest student run technology conference in the midwest, held annually at the
+				University of Illinois at Urbana-Champaign.
+			</div>
+			<a
+				href="https://2022.reflectionsprojections.org/"
+				class="flex flex-row gap-2 w-fit text-black items-center bg-white opacity-80 rounded-md py-2 px-4 my-5 hover:opacity-90 duration-200"
+			>
+				<div>Visit 2022 Site</div>
+				<Icon icon="mdi:arrow-top-right-thin" />
+			</a>
+		</div>
+	</span>
 </div>
