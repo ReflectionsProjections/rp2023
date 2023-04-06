ed79634bd63ea5db1e4e85712b682bedcba27780
Soham Kulkarni <sohamkrox@gmail.com>
2023-04-05 20:43:49 -0500

    Serve sponsor doc on home site

---
+++ src/routes/sponsor-doc/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import PdfViewer from 'svelte-pdf';
+</script>
+
+<div class="m-10">
+	<h1 class=" text-3xl flex text-white justify-center">Reflections | Projections Sponsor Guide</h1>
+	<div class="flex justify-center text-white">
+		<PdfViewer
+			url="../R_P_2023_Sponsor_Doc.pdf"
+			scale={1.3}
+			data={undefined}
+			showBorder={false}
+			showButtons={['navigation', 'zoom', 'print', 'download', 'pageInfo']}
+		/>
+	</div>
+</div>
