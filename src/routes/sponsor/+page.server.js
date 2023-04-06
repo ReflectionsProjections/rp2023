ebe0c383259ba3374e25a1195f04509a7368ae38
Soham Kulkarni <sohamkrox@gmail.com>
2023-04-05 20:45:31 -0500

    Replace old /sponsor route

--- src/routes/sponsor/+page.server.js
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-    throw redirect(302, 'https://drive.google.com/file/d/1FZFJ73xpRquwTWB57BJWjLvXh-tE5hPv/view?usp=sharing');
-}
