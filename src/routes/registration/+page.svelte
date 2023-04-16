299351e35ee9661beacabd05bdd4336640d1679d
Shreya Deshpande <shreyad4@illinois.edu>
2023-04-15 19:38:42 -0500

    registration food mechmania marketing questions

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -84,6 +84,49 @@
                     <input type="text" id="occupation" bind:value={formValues.occupation}>
                 </div>
             {/if}
+
+            <div>
+                <label for="food">Do you have any dietary restrictions? </label>
+                <select id="food" bind:value={formValues.food}>
+                    <option value="Vegetarian">Vegetarian</option>
+                    <option value="Vegan">Vegan</option>
+                    <option value="Gluten-Free">Gluten-Free</option>
+                    <input type="other" id="food-other" bind:value={formValues.foodOther}>
+                </select>
+            </div>
+
+            <div>
+                <label for="mech-puzzle">Are you interested in MechMania/PuzzleBang? </label>
+                <select id="mech-puzzle" bind:value={formValues.mechPuzzle}>
+                    <option value="both">Yes, I'm interested in both</option>
+                    <option value="mechmania">I'm just interested in MechMania</option>
+                    <option value="puzzlebang">I'm just interested in PuzzleBang</option>
+                    <option value="no-Free">No</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="marketing">How did you hear about R|P? </label>
+                <select id="food" bind:value={formValues.food} multiple>
+                    <option value="ACMOH">ACM Open House</option>
+                    <option value="ACMN">ACM Newsletter</option>
+                    <option value="building-ads">Building Ads</option>
+                    <option value="courses">Courses</option>
+                    <option value="WCS">WCS Newsletter</option>
+                    <option value="cs-newsletter">CS Department Newsletter</option>
+                    <option value="social-media">Social Media</option>
+                    <option value="twitter">Twitter</option>
+                    <option value="email">Email</option>
+                    <option value="in-class-pitch">In-Class Pitch</option>
+                    <option value="posters">Posters/Flyers</option>
+                    <option value="quad-day">Quad Day</option>
+                    <option value="e-night">E-night</option>
+                    <option value="website">Website</option>
+                    <option value="slack">Slack</option>
+                    <option value="word-of-mouth">Word of Mouth</option>
+                    <input type="marketingother" id="marketing-other" bind:value={formValues.marketingOther}>
+                </select>
+            </div>
         </form>
     </main>
 </div>
