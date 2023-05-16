e0579160711e5aaa0d913003978ea540fb14da39
Soham Kulkarni <sohamkrox@gmail.com>
2023-05-15 20:34:40 -0500

    Add better 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex flex-col justify-center items-center h-screen">
+	<h1 class="text-white text-4xl font-serif p-5">Oh no, 404!</h1>
+	<h1 class="text-2xl font-serif p-5 text-gray-300">
+		(We couldn't find the page you were looking for.)
+	</h1>
+</div>
