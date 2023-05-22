b91714c68a84624babc65430217619eb1e64bc86
Soham Kulkarni <sohamkrox@gmail.com>
2023-05-21 17:40:17 -0700

    Update college student toggle to be more verbose

--- src/components/registration/is-college-student.svelte
+++ src/components/registration/is-college-student.svelte
@@ -4,34 +4,32 @@
 </script>
 
 <div class="flex flex-col">
-	<label for="is-college-student" class="mx-1 mb-2">Are you a college student? </label>
-	<div
-		class="rounded-md flex flex-row bg-white bg-opacity-5 border-gray-700"
-		id="is-college-student"
-	>
+	<div class="rounded-md flex flex-row bg-white bg-opacity-5 border-gray-700">
 		<button
-			class="w-full p-2 flex justify-center bg-white rounded-l-md {isCollegeStudent === 'yes'
+			class="w-full flex-col items-center gap-1 p-2 flex justify-center bg-white rounded-l-md {isCollegeStudent ===
+			'yes'
 				? 'bg-opacity-20'
 				: 'bg-opacity-0 hover:bg-opacity-10'}"
-			aria-label="yes"
-			title="Yes, I am!"
+			aria-label="I am a college student"
 			on:click={() => {
 				isCollegeStudent = 'yes';
 			}}
 		>
 			<Icon icon="ph:student-fill" class="text-2xl" />
+			<div>I'm a college student</div>
 		</button>
 		<button
-			class="w-full p-2 flex justify-center bg-white rounded-r-md {isCollegeStudent !== 'yes'
+			class="w-full flex-col items-center gap-1 p-2 flex justify-center bg-white rounded-r-md {isCollegeStudent !==
+			'yes'
 				? 'bg-opacity-20'
 				: 'bg-opacity-0 hover:bg-opacity-10'}"
-			aria-label="no"
-			title="No, I'm not"
+			aria-label="I am not a college student"
 			on:click={() => {
 				isCollegeStudent = '';
 			}}
 		>
 			<Icon icon="ph:user-fill" class="text-2xl" />
+			<div>Not a college student</div>
 		</button>
 	</div>
 </div>
