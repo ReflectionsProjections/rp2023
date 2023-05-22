a4d9afc9a1a5c33149200cda35aebc6dd059bf50
Soham Kulkarni <sohamkrox@gmail.com>
2023-05-21 17:33:35 -0700

    Add accessibility controls to page controls

--- src/components/registration/page-controls.svelte
+++ src/components/registration/page-controls.svelte
@@ -8,6 +8,8 @@
 <div class="flex flex-row gap-5 justify-between mx-2">
 	{#if prev >= 0}
 		<button
+			aria-label="previous page"
+			title="previous page"
 			on:click={() => {
 				page = prev;
 			}}><Icon icon="ic:round-navigate-before" class="text-3xl text-white" /></button
@@ -17,6 +19,8 @@
 	{/if}
 	{#if next >= 0}
 		<button
+			aria-label="next page"
+			title="next page"
 			on:click={() => {
 				page = next;
 			}}><Icon icon="ic:round-navigate-next" class="text-3xl text-white" /></button
