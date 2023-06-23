90d381af8de091df1a35e0c73db182ab3388b25f
Saloni Vaishnav <saloniv2@illinois.ed>
2023-06-22 21:23:58 -0500

    set up new route

---
+++ src/routes/carp/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import GlassContainer from '../../components/glass-container.svelte';
+	import IsaCollegeStudent from '../../components/registration/is-college-student.svelte';
+	import PageControls from '../../components/registration/page-controls.svelte';
+
+
+	let page = 0;
+
+
+</script>
+
+<main class="flex h-full">
+
+			<h1> Resume Viewer </h1>
+
+			<div class=">
+				table
+			</div>
+
+</main>
+
+
+<style>
+	input {
+		padding: 0.25rem;
+	}
+</style>
