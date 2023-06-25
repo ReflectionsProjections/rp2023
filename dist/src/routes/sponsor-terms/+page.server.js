9820f0fc71f73fe67968dcd215b91cf5901b7088
Rohan Nunugonda <45941834+rohannunu@users.noreply.github.com>
2023-06-25 09:58:23 -0500

    Added dist to gitignore, removed dist

--- dist/src/routes/sponsor-terms/+page.server.js
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-export function load() {
-    throw redirect(302, '/sponsor.pdf'); // used to be sponsor-terms
-}
-//# sourceMappingURL=+page.server.js.map
--- dist/src/routes/sponsor/+page.server.js
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-export function load() {
-    throw redirect(302, '/sponsor.pdf');
-}
-//# sourceMappingURL=+page.server.js.map
