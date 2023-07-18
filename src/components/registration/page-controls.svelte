56b3c60dd52399904cab18dae2260f55f7dc50b1
kajalpatelinfo <kajal.patel@threekp.com>
2023-07-17 20:40:07 -0600

    Remove debug statements

--- src/components/registration/page-controls.svelte
+++ src/components/registration/page-controls.svelte
@@ -11,20 +11,13 @@
 	$: numPages = Object.keys(pageMeta).length;
   
 	let next: PageIndex | null = pageMeta[page]?.next(isCollegeStudent);
-	console.log("original next:" + next);
 	let prev: PageIndex = pageMeta[page]?.prev(isCollegeStudent);
   
 	$: {
 		const requiredFields = pageMeta[page]?.requiredFields;
 		const isFormComplete = requiredFields && requiredFields.every(field => formValues[field] !== '');
-		console.log("is form complete: " + isFormComplete);
-		console.log('Before: next =', next);
-		console.log('Before: prev =', prev);
-		console.log('page: ', page);
 		next = isFormComplete ? pageMeta[page]?.next(isCollegeStudent) : null;
-		console.log('After: next =', next);
 		prev = pageMeta[page]?.prev(isCollegeStudent);
-		console.log('After: prev =', prev);
 	}
   </script>
   
@@ -52,8 +45,6 @@
 			}
 		  }}
 		><Icon icon="ic:round-navigate-next" class="text-3xl text-white" /></button>
-	  <!-- {:else}
-		<button disabled>Next</button> -->
 	  {/if}
 	{/if}
   </div>
