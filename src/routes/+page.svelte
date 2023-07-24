857c0e2588951f4d05ea3230d9af17ccfa941598
Tiffany <tiyang499@gmail.com>
2023-07-23 22:11:33 -0500

    added prelim schedule component, needs api call fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,81 @@
-<script>
+<script lang='ts'>
 	import Icon from '@iconify/svelte';
 	import svgLogo from '../assets/rp-logo.svg';
 	import ShootingStar from '../components/shooting-star.svelte';
+	import GlassContainer from '../components/glass-container.svelte';
+
+	import { API_URL } from '../constants';
+
+	let day = '';
+	let schedule = {};
+	let day_events_list = {};
+	// Schedule of events; should be in the format of Monday: List_of_events, Tuesday: List_of_events, etc..
+	// Events have: name, description, start_time, duration, location, virtual
+
+	const getSchedule = async () => {
+		const response = await fetch('${API_URL}/events', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const data = await response.json();
+		const schedule = data;
+	};
+
+	function dayButtonClick(event: MouseEvent) {
+		const target = event.currentTarget as HTMLButtonElement;
+		day = target.id;
+		console.log(day);
+
+		// day_events_list = schedule.name_of_day
+		day_events_list = {
+			"Name": "Hackathon",
+			"Description": "a hackathon",
+			"Start Time": "12:00am",
+			"Duration": "48 hours",
+			"Location": "Siebel",
+			"Virtual": "Yes",
+			"Name2": "Hackathon",
+			"Description2": "a hackathon",
+			"Start Time2": "12:00am",
+			"Duration2": "48 hours",
+			"Location2": "Siebel",
+			"Virtual2": "Yes",
+			"Name3": "Hackathon",
+			"Description3": "a hackathon",
+			"Start Time3": "12:00am",
+			"Duration3": "48 hours",
+			"Location3": "Siebel",
+			"Virtual3": "Yes"
+		}
+		console.log(day_events_list);
+	}
+
 </script>
 
+<!-- Put in tmp hero component/Replace with final-->
 <div class="h-full text-white flex items-center">
-	<ShootingStar />
-	<span class="flex flex-col md:flex-row mx-auto p-5 md:bg-white rounded-xl md:bg-opacity-10">
-		<div class="flex flex-col text-2xl m-5 font-serif">
-			<div class="whitespace-nowrap flex flex-row">
-				<div>Reflections | Projections</div>
-			</div>
-			<div class="text-gray-300">2023</div>
-			<div class="text-xl my-3">Coming Soon</div>
-		</div>
-		<div class="flex flex-col m-5 w-96 font-sans">
-			<div class="text-md">
-				The largest student run technology conference in the midwest, held annually at the
-				University of Illinois at Urbana-Champaign.
-			</div>
-			<a
-				href="https://2022.reflectionsprojections.org/"
-				class="flex flex-row gap-2 w-fit items-center bg-white opacity-80 rounded-md py-2 px-4 my-5 hover:opacity-90 duration-200 text-rp-blue"
-			>
-				<div>Visit 2022 Site</div>
-				<Icon icon="mdi:arrow-top-right-thin" />
-			</a>
-		</div>
+	<span class="w-full">
+		<h1 class="w-full text-center text-2xl sm:text-5xl lg:text-7xl mb-6">reflections | projections</h1>
+		<div class="text-center text-md sm:text-2xl lg:text-4xl uppercase mb-6">September 18 - 22, 2023</div>
+		<button class="uppercase bg-white text-black text-center block mx-auto text-sm md:text-md p-4">Register</button>
 	</span>
+</div>
+
+<div class="mx-auto h-[50%] w-[75%] bg-black">
+	<div class="mx-auto flex flex-row bg-white justify-around">
+		<div><button id="Monday" on:click = {dayButtonClick}>Monday</button></div>
+		<div><button id="Tuesday" on:click = {dayButtonClick}>Tuesday</button></div>
+		<div><button id="Wednesday" on:click = {dayButtonClick}>Wednesday</button></div>
+		<div><button id="Friday" on:click = {dayButtonClick}>Friday</button></div>
+	</div>
+	<!-- update this to iterate through the day's event list and disiplay info for each event-->
+	<div class="h-full w-full bg-red-50">
+		<ul>
+			{#each Object.entries(day_events_list) as [key, value]}
+				<li class="mb-2"><strong class="mr-2">{key}:</strong> {value}</li>
+			{/each}
+		</ul>
+	</div>
 </div>
