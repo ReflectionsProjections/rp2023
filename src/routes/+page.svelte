ddc950cc7776b6702fe7e376bc94d097ace7c30e
Aniket Gargya <gargya.aniket@gmail.com>
2023-07-24 20:30:27 -0500

    finish up hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,10 @@
 
 <div class="h-full text-white flex items-center">
 	<span class="w-full">
-		<h1 class="w-full text-center text-2xl sm:text-5xl lg:text-7xl mb-6">reflections | projections</h1>
-		<div class="text-center text-md sm:text-2xl lg:text-4xl uppercase mb-6">September 18 - 22, 2023</div>
-		<button class="uppercase bg-white text-black text-center block mx-auto text-sm md:text-md p-4">Register</button>
+		<h1 class="w-full text-center text-3xl sm:text-5xl lg:text-7xl mb-3 md:mb-6">reflections | projections</h1>
+		<div class="text-center text-md sm:text-2xl lg:text-4xl uppercase mb-5 md:mb-10">September 18 - 22, 2023</div>
+		<button class="bg-white  text-center block mx-auto p-3 lg:p-4 rounded-lg">
+            <span class="uppercase text-black text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-rp-gradient">Register</span>
+        </button>
 	</span>
 </div>
