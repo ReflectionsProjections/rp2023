5849690273b7db049b3415a038c94614024a49b9
kajalpatelinfo <kajal.patel@threekp.com>
2023-07-30 15:14:31 -0600

    Revert submit handler

--- src/components/registration/submit-handler.svelte
+++ src/components/registration/submit-handler.svelte
@@ -2,6 +2,7 @@
 
     export let onSubmit: () => Promise<void>;
     export let fileData: File;
+    // export let attendeeId: string;
   
     const HandleClick = async () => {
       // const attendeeId = await onSubmit();
@@ -16,32 +17,37 @@
         formData.append('file', fileData);
         // formData.append('attendeeId', attendeeId);
 
-            reader.onload = async () => {
-                const blob = new Blob([reader.result as ArrayBuffer]);
+        console.log('File data:', formData);
 
-                const response = await fetch(presignedURL, {
-                    method: 'PUT',
-                    body: blob,
-                    headers: {
-                    'Content-Type': fileData.type,
-                    },
-                });
-
-                return { success: true, message: 'File uploaded successfully' };
-            };
-        } catch (error) {
-            throw new Error('Failed to upload file');
-        }
+        fetch('http://localhost:3000/attendee/upload', {
+        method: 'POST',
+        body: formData 
+        })
+        .then(function(response) {
+            if (response.ok) {
+            return response.text();
+            }
+            throw new Error('Error: ' + response.status);
+        })
+        .then(function(data) {
+            alert('File uploaded successfully.');
+        })
+        .catch(function(error) {
+            alert('An error occurred: ' + error.message);
+        });
       }
     };
+
+    $: {
+        console.log('File data:', fileData);
+    }
   </script>
-    
+  
   <button
     type="submit"
     class="mx-auto disabled:opacity-25 disabled:cursor-not-allowed duration-500 bg-white bg-opacity-30 text-white px-3 py-2 m-3 rounded-md flex gap-2 border border-white"
     id="submit-button"
-    on:click={HandleClick}
-  >
+    on:click={HandleClick}>
     Submit
   </button>
   
