6801ed23e29ac8283532e1d2a7f34ab361909aa7
Aniket Gargya <gargya.aniket@gmail.com>
2023-08-15 18:40:41 -0500

    add qr code to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+    import { API_URL } from '../constants';
+    import { get } from 'svelte/store';
 	import Footer from '../components/footer.svelte';
 	import Sponsors from '../components/home/sponsors.svelte';
+    import GlassContainer from '../components/glass-container.svelte';
+
+    let qrImg = null;
+    const url = get(API_URL);
+    fetch(`${url}/attendee/qr`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+            'Origin': 'dev.reflectionsprojections.org'
+        }
+    }).then(response =>
+        response.text()
+    ).then(text => {
+        qrImg = text; 
+    });
 </script>
 
 <div class="h-full text-white flex items-center">
@@ -19,5 +37,13 @@
 		</button>
 	</span>
 </div>
+<section>
+    <h2 class="text-white text-3xl sm:text-5xl md:text-7xl text-center mb-10 sm:mb-20">Your QR Code</h2>
+    <div class="block w-8/12 max-w-sm mx-auto">
+        <GlassContainer>
+            <img class="w-full aspect-square" src={qrImg} />
+        </GlassContainer>
+    </div>
+</section>
 <Sponsors />
 <Footer />
