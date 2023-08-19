0312c8b20dfa69a465ce9d4bad96e54fd2c832b7
Aniket Gargya <gargya.aniket@gmail.com>
2023-08-19 13:10:05 -0500

    add image alts

--- src/components/home/sponsors.svelte
+++ src/components/home/sponsors.svelte
@@ -9,12 +9,12 @@ const sponsors = [
     <h2 class="text-white text-3xl sm:text-5xl md:text-7xl text-center mb-10 sm:mb-20">Our Sponsors</h2>
     <div class="w-full sm:w-3/4 xl:w-5/8 max-w-4xl mx-auto flex justify-center items-center mb-10 sm:mb-20">
         {#each sponsors[0] as sponsor}
-            <img class="w-6/12" src="logos/{sponsor}.png" />
+            <img class="w-6/12" src="logos/{sponsor}.png" alt="{sponsor} logo" />
         {/each}
     </div>
     <div class="w-3/4 sm:w-1/2 xl:w-3/8 max-w-2xl mx-auto flex justify-around items-center">
         {#each sponsors[1] as sponsor}
-            <img class="w-4/12" src="logos/{sponsor}.png" />
+            <img class="w-4/12" src="logos/{sponsor}.png" alt="{sponsor} logo" />
         {/each}
     </div>
 </div>
