63bf45c2db88fb1949a654718fc8b0fbcd5aa5a1
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-19 18:20:28 -0500

    Make navbar mobile friendly

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -2,60 +2,86 @@
 	import Icon from '@iconify/svelte';
 	import type { User } from '../lib/types';
 	import { userStore } from '../stores/user-store';
+	import { slide } from 'svelte/transition';
 
 	let user: User | null;
 
-    userStore.subscribe(data => user = data);
+	userStore.subscribe((data) => (user = data));
+
+	let openDropDown = false;
 </script>
 
-
 <span class="sticky top-0">
-	<div class="flex px-5 py-5 flex-row justify-between h-24">
-		<a href="/">
+	<div
+		class="flex flex-row justify-between p-5 text-gray-200 text-sm md:text-md lg:text-lg font-sans items-center"
+	>
+		<a href="/" class="flex flex-row items-center gap-3">
 			<img
 				src="/favicon.png"
 				alt="Reflections Projections Home Page"
 				class="hover:rotate-180 duration-500 hover:scale-125 h-[40px] w-[40px] md:h-[50px] md:w-[50px]"
 			/>
-			<img
-				src="/rp-text-logo-white.svg"
-				alt="Reflections Projections Home Page"
-				width={180}
-				class="hidden md:block fill-rp-pink"
-			/>
 		</a>
-		<a
-			href="/speakers"
-			class="hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
-			>Speakers</a
-		>
-		<a
-			href="/speakers"
-			class="hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
-			>Puzzlebang</a
-		>
-		<a
-			href="/speakers"
-			class="hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
-			>MechMania</a
-		>
-
-		{#if user}
-			<div class="flex flex-row gap-3 items-center">
-				<div class="hidden md:flex">{user?.fullName ? `Welcome, ${user.fullName}` : ''}</div>
-				<a href="/preferences">
-					<Icon
-						icon="fluent:person-28-filled"
-						class="text-xl md:text-3xl hover:text-pink-500 duration-200"
-					/>
-				</a>
-			</div>
-		{:else}
+		<div class="flex flex-row justify-evenly w-full md:w-2/3 lg:w-1/2">
 			<a
 				href="/speakers"
 				class="hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
-				>Log In</a
+				>Speakers</a
 			>
-		{/if}
+			<a
+				href="/puzzlebang"
+				class="hidden md:block hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
+				>Puzzlebang</a
+			>
+			<a
+				href="/mechmania"
+				class="hidden md:block hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
+				>MechMania</a
+			>
+			<div
+				class="relative inline-block md:hidden"
+				on:mouseenter={() => {
+					openDropDown = true;
+				}}
+				on:mouseleave={() => {
+					openDropDown = false;
+				}}
+			>
+				<button
+					class="relative md:hidden hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
+					on:click={() => {
+						openDropDown = !openDropDown;
+					}}>Special Events</button
+				>
+				{#if openDropDown}
+					<div class="block absolute z-10 pt-1" in:slide out:slide>
+						<div
+							class="flex flex-col divide-y-2 bg-rp-cream rounded-lg text-rp-blue p-1 border-rp-pink"
+						>
+							<a class="p-1.5 hover:text-rp-dull-pink duration-200" href="/mechmania">MechMania</a>
+							<a class="p-1.5 hover:text-rp-dull-pink duration-200" href="/puzzlebang">Puzzlebang</a
+							>
+						</div>
+					</div>
+				{/if}
+			</div>
+			{#if user}
+				<div class="flex flex-row gap-3 items-center">
+					<div class="hidden md:flex">{user?.fullName ? `Welcome, ${user.fullName}` : ''}</div>
+					<a href="/preferences">
+						<Icon
+							icon="fluent:person-28-filled"
+							class="text-xl md:text-3xl hover:text-pink-500 duration-200"
+						/>
+					</a>
+				</div>
+			{:else}
+				<a
+					href="/login"
+					class="hover:underline underline-offset-8 hover:underline-offset-4 decoration-pink-500 decoration-2 duration-150"
+					>Log In</a
+				>
+			{/if}
+		</div>
 	</div>
 </span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,21 +12,21 @@
 	const url = get(API_URL);
 
 	let user: User | null = null;
-    userStore.subscribe(data => user = data);
+	userStore.subscribe((data) => (user = data));
 
-    console.log(user);
+	console.log(user);
 
 	onMount(() => {
 		try {
-            if (user) {
-                fetch(`${url}/attendee/qr`, {
-                    credentials: 'include'
-                })
-                    .then((response) => response.text())
-                    .then((text) => {
-                        qrImg = text;
-                    });
-            }
+			if (user) {
+				fetch(`${url}/attendee/qr`, {
+					credentials: 'include'
+				})
+					.then((response) => response.text())
+					.then((text) => {
+						qrImg = text;
+					});
+			}
 		} catch (e) {
 			console.error(e);
 		}
@@ -40,27 +40,30 @@
 		<div class="text-center xl:text-center text-md sm:text-2xl lg:text-xl uppercase xl:mb-5 mb-10">
 			September 18 - 22, 2023
 		</div>
-        {#if user != null}
-            <div class="block w-8/12 max-w-sm mx-auto">
-                <GlassContainer>
-                    <img class="w-full aspect-square" src={qrImg} alt="QR Pass" />
-                </GlassContainer>
-            </div>
-        {:else}
-            <p class="w-full md:w-2/3 lg:w-1/2 text-center text-base md:text-lg font-medium text-gray-200">
-                Expand your horizons at the <b class="text-pink-200"
-                    >Midwest’s largest student run tech conference</b
-                > this September. Join us for a week full of insightful talks from industry & academia leaders,
-                hands-on workshops, and networking events.
-            </p>
-            <button
-                class="bg-rp-cream text-center p-4 lg:p-6 my-6 rounded-full hover:bg-opacity-70 duration-300"
-            >
-                <span class="uppercase text-black text-base md:text-xl font-semibold text-rp-gradient"
-                    >Register Now</span
-                >
-            </button>
-        {/if}
+		{#if user != null}
+			<div class="block w-8/12 max-w-sm mx-auto">
+				<GlassContainer>
+					<img class="w-full aspect-square" src={qrImg} alt="QR Pass" />
+				</GlassContainer>
+			</div>
+		{:else}
+			<p
+				class="w-full md:w-2/3 lg:w-1/2 text-center text-base md:text-lg font-medium text-gray-200"
+			>
+				Expand your horizons at the <b class="text-pink-200"
+					>Midwest’s largest student run tech conference</b
+				> this September. Join us for a week full of insightful talks from industry & academia leaders,
+				hands-on workshops, and networking events.
+			</p>
+			<a
+				href="/reister"
+				class="bg-rp-cream text-center p-4 lg:p-6 my-6 rounded-full hover:bg-opacity-70 duration-300"
+			>
+				<span class="uppercase text-black text-base md:text-xl font-semibold text-rp-gradient"
+					>Register Now</span
+				>
+			</a>
+		{/if}
 	</span>
 </div>
 <Sponsors />
