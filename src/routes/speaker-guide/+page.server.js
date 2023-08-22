d75553ad443a2a72225e3d48794eadcc273b2017
Tiffany <tiyang499@gmail.com>
2023-08-22 18:15:08 -0500

    Added photos, adjusted padding, created svelte component, updated to include keynote speaker, fixed color

--- src/routes/speaker-guide/+page.server.js
+++ src/routes/speaker-guide/+page.server.js
@@ -1,5 +1,5 @@
-import { redirect } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
 
-export function load() {
-	throw redirect(302, '/speaker-guide.pdf');
-}
+// export function load() {
+// 	throw redirect(302, '/speaker-guide.pdf');
+// }
