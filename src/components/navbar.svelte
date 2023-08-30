4749fe84db2efb79fed1f625155fe1fac5b4e47c
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-30 15:17:12 -0500

    Update navbar with gradient background

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -27,7 +27,7 @@
 <nav class="sticky block top-0 z-50">
 	<div
 		class="flex flex-row justify-between p-5 text-white text-sm md:text-md lg:text-lg font-sans items-center {y >
-			50 && 'bg-rp-blue bg-opacity-25 backdrop-blur-sm'} "
+			50 && 'navbar-bg '} duration-300"
 	>
 		<a href="/" class="flex flex-row items-center gap-3">
 			<img
@@ -98,3 +98,10 @@
 		</div>
 	</div>
 </nav>
+
+<style>
+	.navbar-bg {
+		background: rgb(29, 46, 91);
+		background: linear-gradient(180deg, rgba(29, 46, 91, 1) 0%, rgba(255, 255, 255, 0) 100%);
+	}
+</style>
