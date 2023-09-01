ccd626c51b3d128054447859fd76b48da58eeb97
Soham Kulkarni <sohamkrox@gmail.com>
2023-09-01 01:01:19 -0500

    Add statistics to front page

---
+++ src/components/home/stats.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Countup from 'svelte-countup';
+	import CountUp from '$lib/util/countup.svelte';
+
+	const DURATION_MS = 1000;
+</script>
+
+<div
+	class="w-[90%] md:w-4/5 lg:w-4/5 xl:w-3/5
+    flex flex-row gap-5 justify-between items-center my-14 font-serif
+    overflow-x-clip
+    text-base sm:text-xl md:text-2xl lg:text-3xl"
+>
+	<div class="flex flex-col items-center">
+		<CountUp start={0} end={5} duration={DURATION_MS} />
+		<!-- <CountUp start={0} end={5} duration={DURATION_MS} /> -->
+		<p>Days</p>
+	</div>
+	<div class="flex flex-col items-center">
+		<span class="flex flex-row">
+			<CountUp start={0} end={11} duration={DURATION_MS} />
+			<p>+</p>
+		</span>
+		<p>Speakers</p>
+	</div>
+	<div class="flex flex-col items-center">
+		<span class="flex flex-row">
+			<CountUp start={0} end={2500} duration={DURATION_MS} step={137} />
+			<p>+</p>
+		</span>
+		<p>Attendees</p>
+	</div>
+	<div class="flex flex-col items-center">
+		<span class="flex flex-row">
+			<CountUp start={0} end={13} duration={DURATION_MS} />
+			<p>+</p>
+		</span>
+		<p>Companies</p>
+	</div>
+</div>
---
+++ src/lib/util/countup.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let start: number = 0;
+	export let end: number;
+	export let duration: number = 1000;
+	export let step: number = 1;
+
+	let value: number;
+
+	const increment = () => {
+		value = value + step;
+		if (value < end) {
+			const dur = duration / ((end - start) / step);
+			setTimeout(increment, dur);
+		} else {
+			value = end;
+		}
+	};
+
+	onMount(() => {
+		value = start;
+		setTimeout(increment, 300);
+	});
+</script>
+
+<p>
+	{value}
+</p>
